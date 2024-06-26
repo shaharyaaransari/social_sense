4f36de2c4a2c4cf4206c76834740f859d73fd8a3
shaharyaaransari <shaharyaaransari786@gmail.com>
2024-06-27 00:25:57 +0530

    frame2 Transaction Completed

--- social_sense/app/frame2/page.js
+++ social_sense/app/frame2/page.js
@@ -2,6 +2,7 @@ import Card from '@/components/Frame2/Card'
 import DoughnutChart from '@/components/Frame2/Doughnut'
 import LineChart from '@/components/Frame2/LineChart'
 import Navbar from '@/components/Frame2/Navbar'
+import TransactionSection from '@/components/Frame2/Transaction'
 import React from 'react'
 
 export default function Dashboard() {
@@ -9,10 +10,14 @@ export default function Dashboard() {
     <div>
        <Navbar/> 
          <Card/>
-          <div className=" bg-slate-100 flex ">
+          <div className=" bg-slate-100 flex p-4">
             <LineChart/>
               <DoughnutChart/>
           </div>
+           <div className=" bg-slate-100">
+           <TransactionSection/>
+           </div>
+            
     </div>
   )
 }
---
+++ social_sense/components/ui/CustomerData.js
@@ -0,0 +1,189 @@
+const customersData = [
+    {
+      name: "John Doe",
+      profile: "http://example.com/profiles/johndoe",
+      avatar: "https://tse1.mm.bing.net/th?id=OIP.vp0M4INdNNtA5cvoWYamfQHaEK&pid=Api&P=0&h=180",
+      email: "john.doe@example.com",
+      age: 28,
+      bankAccount: "Visa",
+      transactions: [
+        {
+          invoiceNumber: "INV-1001",
+          dateOfBill: "2024-01-15",
+          amount: 250.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1006",
+          dateOfBill: "2024-05-10",
+          amount: 300.00,
+          status: "Pending"
+        }
+        
+      ]
+    },
+    {
+      name: "Jane Smith",
+      profile: "http://example.com/profiles/janesmith",
+      avatar: "https://tse2.mm.bing.net/th?id=OIP.QYWEiWM_zD5A4S8EqSE-fAHaEX&pid=Api&P=0&h=180",
+      email: "jane.smith@example.com",
+      age: 34,
+      bankAccount: "MasterCard",
+      transactions: [
+        
+        {
+          invoiceNumber: "INV-1007",
+          dateOfBill: "2024-06-15",
+          amount: 400.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1012",
+          dateOfBill: "2024-10-22",
+          amount: 600.00,
+          status: "Pending"
+        }
+      ]
+    },
+    {
+      name: "Robert Johnson",
+      profile: "http://example.com/profiles/robertjohnson",
+      avatar: "https://tse2.mm.bing.net/th?id=OIP.d1JR_y-cdTEMRbVgeZOpiAHaEK&pid=Api&P=0&h=180",
+      email: "robert.johnson@example.com",
+      age: 42,
+      bankAccount: "Visa",
+      transactions: [
+        {
+          invoiceNumber: "INV-1003",
+          dateOfBill: "2024-03-10",
+          amount: 150.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1008",
+          dateOfBill: "2024-07-08",
+          amount: 200.00,
+          status: "Pending"
+        }
+      ]
+    },
+    {
+      name: "Emily Davis",
+      profile: "http://example.com/profiles/emilydavis",
+      avatar: "https://tse3.mm.bing.net/th?id=OIP.f_fwoLfc6c6NcwTNd7-qvAHaDt&pid=Api&P=0&h=180",
+      email: "emily.davis@example.com",
+      age: 29,
+      bankAccount: "American Express",
+      transactions: [
+        {
+          invoiceNumber: "INV-1004",
+          dateOfBill: "2024-04-05",
+          amount: 350.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1009",
+          dateOfBill: "2024-08-20",
+          amount: 300.00,
+          status: "Paid"
+        }
+      ]
+    },
+    {
+      name: "Michael Brown",
+      profile: "http://example.com/profiles/michaelbrown",
+      avatar: "https://tse2.mm.bing.net/th?id=OIP.I100Ino_tKDlbFuTtptxLAHaEK&pid=Api&P=0&h=180",
+      email: "michael.brown@example.com",
+      age: 37,
+      bankAccount: "PayPal",
+      transactions: [
+        {
+          invoiceNumber: "INV-1005",
+          dateOfBill: "2024-05-22",
+          amount: 600.00,
+          status: "Pending"
+        },
+        {
+          invoiceNumber: "INV-1010",
+          dateOfBill: "2024-09-05",
+          amount: 450.00,
+          status: "Paid"
+        }
+      ]
+    },
+    {
+      name: "Sarah Johnson",
+      profile: "http://example.com/profiles/sarahjohnson",
+      avatar: "https://tse2.mm.bing.net/th?id=OIP.I100Ino_tKDlbFuTtptxLAHaEK&pid=Api&P=0&h=180",
+      email: "sarah.johnson@example.com",
+      age: 39,
+      bankAccount: "MasterCard",
+      transactions: [
+        {
+          invoiceNumber: "INV-1017",
+          dateOfBill: "2024-01-08",
+          amount: 280.00,
+          status: "Pending"
+        },
+        {
+          invoiceNumber: "INV-1022",
+          dateOfBill: "2024-06-20",
+          amount: 400.00,
+          status: "Paid"
+        }
+      ]
+    },
+    {
+      name: "Alexandra Brown",
+      profile: "http://example.com/profiles/alexandrabrown",
+      avatar: "https://tse1.mm.bing.net/th?id=OIP.vp0M4INdNNtA5cvoWYamfQHaEK&pid=Api&P=0&h=180",
+      email: "alexandra.brown@example.com",
+      age: 31,
+      bankAccount: "Visa",
+      transactions: [
+        {
+          invoiceNumber: "INV-1018",
+          dateOfBill: "2024-02-12",
+          amount: 200.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1023",
+          dateOfBill: "2024-07-30",
+          amount: 500.00,
+          status: "Pending"
+        }
+      ]
+    },
+    {
+      name: "David Smith",
+      profile: "http://example.com/profiles/davidsmith",
+      avatar: "https://tse3.mm.bing.net/th?id=OIP.f_fwoLfc6c6NcwTNd7-qvAHaDt&pid=Api&P=0&h=180",
+      email: "david.smith@example.com",
+      age: 33,
+      bankAccount: "American Express",
+      transactions: [
+        {
+          invoiceNumber: "INV-1019",
+          dateOfBill: "2024-03-25",
+          amount: 300.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1024",
+          dateOfBill: "2024-08-12",
+          amount: 450.00,
+          status: "Paid"
+        },
+        {
+          invoiceNumber: "INV-1029",
+          dateOfBill: "2024-12-18",
+          amount: 280.00,
+          status: "Pending"
+        }
+      ]
+    }
+  ];
+  
+  export default customersData;
+  
